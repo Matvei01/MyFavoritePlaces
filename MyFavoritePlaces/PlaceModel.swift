c77cd8d84efae476eeb3f2d694c1295fc94c8921
Matvei95 <khlestov.95@mail.ru>
2022-06-13 19:30:56 +0500

    Working with Model

---
+++ MyFavoritePlaces/PlaceModel.swift
@@ -0,0 +1,33 @@
+//
+//  PlaceModel.swift
+//  MyFavoritePlaces
+//
+//  Created by Matvei Khlestov on 13.06.2022.
+//
+
+import Foundation
+
+struct Place {
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+    static let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
+    static func getPlaces() -> [Place] {
+        
+        var places = [Place]()
+        
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "Челябинск", type: "Ресторан", image: place))
+        }
+        
+        return places
+    }
+}
