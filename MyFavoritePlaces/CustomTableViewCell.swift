eea409678f6bf9e20fb3b914bb4e8b4d43a2e937
Matvei95 <khlestov.95@mail.ru>
2022-06-13 18:32:20 +0500

    Custom Table View Cell

---
+++ MyFavoritePlaces/CustomTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomTableViewCell.swift
+//  MyFavoritePlaces
+//
+//  Created by Matvei Khlestov on 13.06.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
